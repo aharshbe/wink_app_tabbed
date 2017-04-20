a6c51cd22e07eacc94e2e0e2b3391c4ab3130234
Austin Harshberger <aharshberger@sf-m-aharshberger.ideo.com>
2017-04-19 17:05:31 -0700

    adding changes

--- MyApplication2/app/src/main/java/com/example/aharshberger/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/aharshberger/myapplication/MainActivity.java
@@ -77,16 +77,6 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         pref = getApplicationContext().getSharedPreferences("prefs", MODE_PRIVATE);
         editor_names = pref.edit();
 
